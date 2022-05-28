5cf1cbcc246a5e29e04a4ce212ee14440c1386ac
Ashraf <hassasraf@gmail.com>
2022-05-28 11:14:56 +0600

    Added main pagination functionality

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
 const paginate = (followers) => {
-    const itemsPerPage = 9
-    const pages = Math.ceil(followers.length / itemsPerPage);
-
-    const newFollowers = Array.from({length:pages}, (_ ,index ) => {
-        const start =  index * itemsPerPage
-        return followers.slice(start, start + itemsPerPage)
+    const itemsPerPage = 10
+    const numberOfPages = Math.ceil(followers.length / itemsPerPage)
+  
+    const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+      const start = index * itemsPerPage
+      return followers.slice(start, start + itemsPerPage)
     })
-
+  
     return newFollowers
-}
-
-export default paginate
+  }
+  
+  export default paginate
